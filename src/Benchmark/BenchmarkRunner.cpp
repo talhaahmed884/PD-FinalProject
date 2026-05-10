b23c149b4ae074e8540579993b4319156db202b6
Talha Ahmed <talhaahmed884@gmail.com>
2026-05-09 19:08:32 -0700

    Minor fixes

--- src/Benchmark/BenchmarkRunner.cpp
+++ src/Benchmark/BenchmarkRunner.cpp
@@ -102,6 +102,12 @@ BenchmarkResult BenchmarkRunner::benchmarkOpenMP(const Board &board, const strin
     OpenMPSolver solver(threads);
 
 #ifdef _OPENMP
+#pragma omp parallel
+    {
+    }
+#endif
+
+#ifdef _OPENMP
     const double startSec = omp_get_wtime();
     solver.solve(copy);
     const double timeSec = omp_get_wtime() - startSec;
